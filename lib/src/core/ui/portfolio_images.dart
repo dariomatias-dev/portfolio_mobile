19165e8a23d8b41a7664ad7cc0f9d094b295d19c
dario <matiasdario75@gmail.com>
2023-08-26 17:48:40 -0300

    feat: added to top of home screen
    - Created the elements and styles corresponding to the upper part of the home screen.

--- lib/src/core/ui/portfolio_images.dart
+++ lib/src/core/ui/portfolio_images.dart
@@ -1,8 +1,10 @@
 class PortfolioImages {
   static const String _path = 'assets/images';
 
+  static String myPhoto = _getPath('my_photo.png');
   static String logo = _getPath('logo.png');
   static String backgroundImageChair = _getPath('background_image_chair.jpg');
+  static String backgroundProfile = _getPath('background_profile.png');
 
   static String _getPath(String fileName) {
     return '$_path/$fileName';
--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,21 +1,77 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/src/core/ui/portfolio_images.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: Text(
-          'Home',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
+      body: Stack(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              SizedBox(
+                height: 120.0,
+                width: double.infinity,
+                child: Image.asset(
+                  PortfolioImages.backgroundProfile,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  padding: const EdgeInsets.only(
+                    top: 50,
+                  ),
+                  alignment: Alignment.centerLeft,
+                  decoration: const BoxDecoration(
+                    color: Colors.white12,
+                    borderRadius: BorderRadius.vertical(
+                      top: Radius.circular(10),
+                    ),
+                  ),
+                  child: const Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 20,
+                    ),
+                    child: Column(
+                      children: [
+                        Text(
+                          'Dário Matias',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ),
+          Positioned(
+            top: 78.0,
+            left: 20.0,
+            child: ClipOval(
+              child: Container(
+                padding: const EdgeInsets.all(2.0),
+                decoration: const BoxDecoration(
+                  color: Colors.black,
+                ),
+                child: Image.asset(
+                  PortfolioImages.myPhoto,
+                  width: 80,
+                  height: 80,
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
