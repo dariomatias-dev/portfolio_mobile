7a30d3b2e929064b32798db226976489454a3bbc
dario <matiasdario75@gmail.com>
2023-08-28 16:20:57 -0300

    feat(HomeScreen): Added SafeArea to HomeScreen and started adding first contents
    - Implemented SafeArea on HomeScreen to ensure a safe area free from obstructions.
    - Started the process of adding the first content elements to the canvas.

--- lib/src/screens/home_screen.dart
+++ lib/src/screens/home_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:portfolio/src/core/ui/portfolio_colors.dart';
+
 import 'package:portfolio/src/core/ui/portfolio_images.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -7,86 +7,122 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const double contentHeight = 120.0;
+    final double heightAvailable =
+        MediaQuery.sizeOf(context).height - contentHeight;
+
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: SingleChildScrollView(
-        child: Stack(
-          children: [
-            Stack(
-              children: <Widget>[
-                Column(
-                  children: [
-                    SizedBox(
-                      height: 130.0,
-                      width: double.maxFinite,
-                      child: Image.asset(
-                        PortfolioImages.backgroundProfile,
-                        fit: BoxFit.cover,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Stack(
+            children: [
+              Stack(
+                children: <Widget>[
+                  Column(
+                    children: [
+                      SizedBox(
+                        height: 130.0,
+                        width: double.maxFinite,
+                        child: Image.asset(
+                          PortfolioImages.backgroundProfile,
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                    ),
-                    Container(
-                      height: MediaQuery.sizeOf(context).height,
-                      decoration: BoxDecoration(
-                        color: PortfolioColors.backgroundColor,
+                      Container(
+                        height: heightAvailable,
                       ),
-                    ),
-                  ],
-                ),
-                Positioned(
-                  top: 120.0,
-                  right: 0,
-                  bottom: 0,
-                  left: 0,
-                  child: Container(
-                    padding: const EdgeInsets.only(
-                      top: 50.0,
-                    ),
-                    alignment: Alignment.centerLeft,
-                    decoration: BoxDecoration(
-                      color: PortfolioColors.backgroundColor,
-                      borderRadius: const BorderRadius.vertical(
-                        top: Radius.circular(10.0),
+                    ],
+                  ),
+                  Positioned(
+                    top: contentHeight,
+                    right: 0,
+                    bottom: 0,
+                    left: 0,
+                    child: Container(
+                      padding: const EdgeInsets.only(
+                        top: 60.0,
                       ),
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: 20.0,
+                      decoration: const BoxDecoration(
+                        color: Colors.black,
+                        borderRadius: BorderRadius.vertical(
+                          top: Radius.circular(10.0),
+                        ),
                       ),
-                      child: Column(
-                        children: [
-                          Text(
-                            'Dário Matias',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 14,
-                              fontWeight: FontWeight.w400,
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 20.0,
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Dário Matias',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                              ),
                             ),
-                          ),
-                        ],
+                            SizedBox(height: 4.0),
+                            Row(
+                              children: [
+                                Icon(
+                                  Icons.location_on_outlined,
+                                  color: Colors.white54,
+                                ),
+                                Text(
+                                  'Areial, PB - Brasil',
+                                  style: TextStyle(
+                                    color: Colors.white54,
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 10.0),
+                            Text(
+                              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+                              textAlign: TextAlign.justify,
+                              softWrap: true,
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            ),
+                            SizedBox(height: 20.0),
+                            Divider(),
+                            SizedBox(height: 20.0),
+                            Text(
+                              'oi',
+                              style: TextStyle(
+                                color: Colors.white,
+                              ),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Positioned(
-              top: 78.0,
-              left: 20.0,
-              child: Container(
-                padding: const EdgeInsets.all(4.0),
-                decoration: BoxDecoration(
-                  color: PortfolioColors.backgroundColor,
-                  shape: BoxShape.circle,
-                ),
-                child: Image.asset(
-                  PortfolioImages.myPhoto,
-                  width: 80.0,
-                  height: 80.0,
-                  fit: BoxFit.cover,
+                ],
+              ),
+              Positioned(
+                top: 78.0,
+                left: 20.0,
+                child: Container(
+                  padding: const EdgeInsets.all(4.0),
+                  decoration: const BoxDecoration(
+                    color: Colors.black,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Image.asset(
+                    PortfolioImages.myPhoto,
+                    width: 80.0,
+                    height: 80.0,
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
