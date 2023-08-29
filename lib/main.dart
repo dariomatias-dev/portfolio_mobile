89d3d8377ce17ab318f39f84223ad6c88889e1c5
dario <matiasdario75@gmail.com>
2023-08-29 10:28:24 -0300

    feat: .env added and implemented
    - Added .env in the application, providing greater security when saving passwords and URLs.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'package:portfolio/src/portfolio_app.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: '.env');
+
   runApp(
     const PortfolioApp(),
   );
