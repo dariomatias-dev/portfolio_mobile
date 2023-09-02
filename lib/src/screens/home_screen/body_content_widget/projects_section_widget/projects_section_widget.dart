48050013247e2ab177cea94436bfdc37bf5edc9b
dario <matiasdario75@gmail.com>
2023-09-02 12:46:59 -0300

    feat: add image to project section title
    - Added image in the title of the projects section, for illustrative purposes only.

--- lib/src/screens/home_screen/body_content_widget/projects_section_widget/projects_section_widget.dart
+++ lib/src/screens/home_screen/body_content_widget/projects_section_widget/projects_section_widget.dart
@@ -25,8 +25,10 @@ class _ProjectsSectionWidgetState extends State<ProjectsSectionWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 170.0,
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        vertical: 10.0,
+      ),
       child: FutureBuilder(
           future: fetchData(),
           builder: (context, snapshot) {
@@ -35,20 +37,57 @@ class _ProjectsSectionWidgetState extends State<ProjectsSectionWidget> {
             Widget contentWidget(List<dynamic> data) {
               final projects = data.cast<ProjectModel>();
 
-              return ListView.separated(
-                itemCount: projects.length,
-                scrollDirection: Axis.horizontal,
-                separatorBuilder: (context, index) {
-                  return const SizedBox(width: 20.0);
-                },
-                itemBuilder: (context, index) {
-                  final project = projects[index];
+              return Column(
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                    height: 100.0,
+                    child: Stack(
+                      children: [
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(16.0),
+                          child: Opacity(
+                            opacity: 0.6,
+                            child: Image.network(
+                              'https://c0.wallpaperflare.com/preview/524/860/912/screen-code-coding-programming.jpg',
+                              fit: BoxFit.cover,
+                              width: double.infinity,
+                            ),
+                          ),
+                        ),
+                        const Center(
+                          child: Text(
+                            'Projetos',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 18.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 40.0),
+                  SizedBox(
+                    height: 170.0,
+                    child: ListView.separated(
+                      itemCount: projects.length,
+                      scrollDirection: Axis.horizontal,
+                      separatorBuilder: (context, index) {
+                        return const SizedBox(width: 20.0);
+                      },
+                      itemBuilder: (context, index) {
+                        final project = projects[index];
 
-                  return ProjectCardWidget(
-                    projectName: project.name,
-                    projectImageUrl: project.files[0].url,
-                  );
-                },
+                        return ProjectCardWidget(
+                          projectName: project.name,
+                          projectImageUrl: project.files[0].url,
+                        );
+                      },
+                    ),
+                  ),
+                ],
               );
             }
 
