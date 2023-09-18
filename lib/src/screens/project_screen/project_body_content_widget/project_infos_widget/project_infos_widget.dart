dc1dcdc8652d4b7897420eea309f47994a7b3109
dario <matiasdario75@gmail.com>
2023-09-18 18:52:47 -0300

    feat(ProjectScreen: button to access the source code of the project development area added
    - Added button to access repository with the project development area code.

--- lib/src/screens/project_screen/project_body_content_widget/project_infos_widget/project_infos_widget.dart
+++ lib/src/screens/project_screen/project_body_content_widget/project_infos_widget/project_infos_widget.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:portfolio/src/core/ui/elevated_button_style.dart';
+import 'package:portfolio/src/core/ui/helpers/open_url.dart';
+
 import 'package:portfolio/src/providers/project_data_provider_inherited_widget.dart';
 
 import 'package:portfolio/src/screens/project_screen/project_body_content_widget/project_infos_widget/project_infos_details_widget.dart';
@@ -14,16 +17,20 @@ class ProjectInfosWidget extends StatelessWidget {
         ProjectDataProviderInheritedWidget.of(context)!.developmentArea;
     late final String projectDescription;
     late final List<String> projectDetails;
+    late final String sourceCode;
 
     if (developmentArea == 'frontend') {
       projectDescription = project.frontend!.description;
       projectDetails = project.frontend!.details;
+      sourceCode = project.frontend!.sourceCode;
     } else if (developmentArea == 'backend') {
       projectDescription = project.backend!.description;
       projectDetails = project.backend!.details;
+      sourceCode = project.backend!.sourceCode;
     } else {
       projectDescription = project.mobile!.description;
       projectDetails = project.mobile!.details;
+      sourceCode = project.mobile!.sourceCode;
     }
 
     return Column(
@@ -50,6 +57,26 @@ class ProjectInfosWidget extends StatelessWidget {
         ProjectInfosDetailsWidget(
           details: projectDetails,
         ),
+        const SizedBox(height: 10.0),
+        const Divider(),
+        const SizedBox(height: 10.0),
+        SizedBox(
+          width: double.infinity,
+          height: 44.0,
+          child: ElevatedButton(
+            onPressed: () {
+              OpenUrl(
+                context: context,
+              ).launch(sourceCode);
+            },
+            style: ElevatedButtonStyle.defaultStyle.copyWith(
+              backgroundColor: MaterialStatePropertyAll(
+                Colors.grey.withOpacity(0.1),
+              ),
+            ),
+            child: const Text('Código fonte'),
+          ),
+        )
       ],
     );
   }
